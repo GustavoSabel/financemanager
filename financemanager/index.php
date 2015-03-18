7c28aa01bf9aaaed2abd1d4a56b8260a652a60f3
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2015-03-18 19:32:15 -0300

    Criada a página de cadastro de categirua

--- financemanager/index.php
+++ financemanager/index.php
@@ -5,6 +5,7 @@
   </head>
   <body>
     <h1>Finance Manager</h1>
-    <a href="view/cadastrousuario.php">Cadastro de usuário</a>
+    <a href="view/cadastrousuario.php">Cadastro de usuário</a><br>
+	<a href="view/cadastrocategoria.php">Cadastro de categoria</a>
   </body>
 </html>
