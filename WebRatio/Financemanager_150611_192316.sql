51b80b2ee63b6a029a4f136a138e4ac8f9dd02c7
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2015-06-11 19:40:27 -0300

    Criada uma imagem do UML e o script do banco

---
+++ WebRatio/Financemanager_150611_192316.sql
@@ -0,0 +1,109 @@
+-- Group [Group]
+create table `group` (
+   `group`  integer  not null,
+   `groupname`  varchar(255),
+  primary key (`oid`)
+);
+
+
+-- Module [Module]
+create table `module` (
+   `oid`  integer  not null,
+   `moduleid`  varchar(255),
+   `modulename`  varchar(255),
+  primary key (`oid`)
+);
+
+
+-- User [User]
+create table `user` (
+   `oid`  integer  not null,
+   `email`  varchar(255),
+   `password`  varchar(255),
+   `username`  varchar(255),
+  primary key (`oid`)
+);
+
+
+-- Usuario [ent1]
+create table `usuario` (
+   `idusuario`  not null auto_increment,
+   `nome`  varchar(255),
+   `login`  varchar(255),
+   `senha`  varchar(255),
+  primary key (`idusuario`)
+);
+
+
+-- Categoria [ent3]
+create table `categoria` (
+   `oid`  integer  not null,
+   `descricao`  varchar(255),
+  primary key (`oid`)
+);
+
+
+-- Parcela [ent4]
+create table `transacao` (
+   `oid`  integer  not null,
+   `valor`  decimal(19,2),
+   `datapagamento`  date,
+   `pago`  bit,
+     date,
+  primary key (`oid`)
+);
+
+
+-- Transação [ent5]
+create table `parcelas` (
+   `oid`  integer  not null,
+   `valortotal`  decimal(19,2),
+   `numeroparcelas`  integer,
+     integer,
+     varchar(255),
+  primary key (`oid`)
+);
+
+
+-- Group_DefaultModule [Group2DefaultModule_DefaultModule2Group]
+alter table `group`  add column  `module_oid`  integer;
+alter table `group`   add index fk_group_module (`module_oid`), add constraint fk_group_module foreign key (`module_oid`) references `module` (`oid`);
+create index `idx_group_module` on `group`(`module_oid`);
+
+
+-- Group_Module [Group2Module_Module2Group]
+create table `group_module` (
+   `group_oid`  integer not null,
+   `module_oid`  integer not null,
+  primary key (`group_oid`, `module_oid`)
+);
+alter table `group_module`   add index fk_group_module_group (`group_oid`), add constraint fk_group_module_group foreign key (`group_oid`) references `group` (`oid`);
+alter table `group_module`   add index fk_group_module_module (`module_oid`), add constraint fk_group_module_module foreign key (`module_oid`) references `module` (`oid`);
+create index `idx_group_module_group` on `group_module`(`group_oid`);
+create index `idx_group_module_module` on `group_module`(`module_oid`);
+
+
+-- User_DefaultGroup [User2DefaultGroup_DefaultGroup2User]
+alter table `user`  add column  `group_oid`  integer;
+alter table `user`   add index fk_user_group (`group_oid`), add constraint fk_user_group foreign key (`group_oid`) references `group` (`oid`);
+create index `idx_user_group` on `user`(`group_oid`);
+
+
+-- User_Group [User2Group_Group2User]
+create table `user_group` (
+   `user_oid`  integer not null,
+   `group_oid`  integer not null,
+  primary key (`user_oid`, `group_oid`)
+);
+alter table `user_group`   add index fk_user_group_user (`user_oid`), add constraint fk_user_group_user foreign key (`user_oid`) references `user` (`oid`);
+alter table `user_group`   add index fk_user_group_group (`group_oid`), add constraint fk_user_group_group foreign key (`group_oid`) references `group` (`oid`);
+create index `idx_user_group_user` on `user_group`(`user_oid`);
+create index `idx_user_group_group` on `user_group`(`group_oid`);
+
+
+-- Categoria_Parcelas [rel9]
+alter table `categoria`  add column  `parcelas_oid`  integer;
+alter table `categoria`   add index fk_categoria_parcelas (`parcelas_oid`), add constraint fk_categoria_parcelas foreign key (`parcelas_oid`) references `parcelas` (`oid`);
+create index `idx_categoria_parcelas` on `categoria`(`parcelas_oid`);
+
+
